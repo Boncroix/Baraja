acbc5cd9a481591cc6ad5597a4d588f4832371f4
Boncroix <boncroix@gmail.com>
2023-08-01 20:43:56 +0200

    feat: funciones crear baraja y barajar creadas y funcionando

--- baraja.py
+++ baraja.py
@@ -1,9 +1,24 @@
 def crearBaraja():
     palos = ['O', 'C', 'E', 'B']
     cartas = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
-    baraja = {}
+    baraja = []
 
     for palo in palos:
         for carta in cartas:
-            baraja.add(carta + palo)
+            baraja.append(carta + palo)
     return baraja
+
+
+baraja = (crearBaraja())
+
+
+def barajar(baraja):
+    baraja_barajada = set()
+    for carta in baraja:
+        baraja_barajada.add(carta)
+    return baraja_barajada
+
+
+baraja_barajada = (barajar(baraja))
+print(baraja_barajada)
+print(len(baraja_barajada))
